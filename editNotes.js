34797cb91e871ef9b6f097aa67e8a4d6a7787852
Mateo-Max <max.mateo.02@gmail.com>
2020-11-06 20:31:18 -0300

    first commit

---
+++ editNotes.js
@@ -0,0 +1,63 @@
+const editNoteForm = document.getElementById('editNoteForm')
+const editNoteSelect = document.getElementById('editNoteSelect')
+const editNoteName = document.getElementById('editNoteName')
+const editNoteTextarea = document.getElementById('editNoteTextarea')
+var editingNoteId = ''
+
+
+function displayEditModal(noteId) {
+    $('#editNoteModal').modal('show')
+
+
+    const usersNotes = JSON.parse(localStorage.getItem('usersNotes')) || [];
+    const editNotes = usersNotes.filter(n => n.id == noteId)
+
+    var noteName = editNotes[0].noteName
+    var noteSelect = editNotes[0].noteSelect
+    var noteTextarea = editNotes[0].noteTextarea
+
+    editNoteName.value = noteName
+    editNoteSelect.value = noteSelect
+    editNoteTextarea.value = noteTextarea
+
+    editingNoteId = noteId
+    
+}
+
+editNoteForm.onsubmit = (e) => {
+    e.preventDefault()
+    const newName = editNoteName.value
+    const newSelect = editNoteSelect.value
+    const newTextarea = editNoteTextarea.value
+
+    const usersNotes = JSON.parse(localStorage.getItem('usersNotes')) || [];
+    const editNotes = usersNotes.map((n) => {
+        if (editingNoteId === n.id) {
+            const userNote = {
+                ...n,
+                noteName: newName,
+                noteSelect: newSelect,
+                noteTextarea: newTextarea
+            }
+            return userNote
+        } else {
+            return n
+        }
+    })
+
+    const editedNote = JSON.stringify(editNotes)
+    localStorage.setItem('usersNotes',editedNote)
+    $('#editNoteModal').modal('hide')
+    createInfoModals()
+    displayNotes()
+}
+
+function deleteUser(noteId) {
+    const usersNotes = JSON.parse(localStorage.getItem('usersNotes')) || [];
+    const deletingNote = usersNotes.filter ((n) => noteId !== n.id) 
+
+    const noteThatStay = JSON.stringify(deletingNote)
+    localStorage.setItem('usersNotes',noteThatStay)
+    createInfoModals()
+    displayNotes()
+}
---
+++ takeAndDisplayNotes.js
@@ -0,0 +1,109 @@
+const addNoteForm = document.getElementById('addNoteForm');
+const addNoteName = document.getElementById('addNoteName');
+const addNoteSelect = document.getElementById('addNoteSelect');
+const addNoteTextarea = document.getElementById('addNoteTextarea');
+const notesTable = document.getElementById('notesTable')
+const infoModals = document.getElementById('infoModals')
+
+
+
+
+const generateId = function () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+};
+
+addNoteForm.onsubmit = (e) => {
+    e.preventDefault();
+    const usersNotes = JSON.parse(localStorage.getItem('usersNotes')) || [];
+
+    const noteName = addNoteName.value;
+    const noteSelect = addNoteSelect.value;
+    const noteTextarea = addNoteTextarea.value;
+    addNoteForm.reset();
+    
+    
+    usersNotes.push ({
+        noteName,
+        noteSelect,
+        noteTextarea,
+        id: generateId()
+    })
+
+    const usersNotesJson = JSON.stringify(usersNotes);
+    localStorage.setItem('usersNotes',usersNotesJson);
+    $('#addNoteModal').modal('hide')
+    displayNotes()
+    createInfoModals()
+}
+
+function displayNotes() {
+    const usersNotes = JSON.parse(localStorage.getItem('usersNotes')) || [];
+    const row = []
+    for (let i = 0; i < usersNotes.length; i++) {
+        const usernote = usersNotes[i];
+
+        const tr = `
+        <tr>
+            <th scope="row">
+                <button onclick="deleteUser('${usernote.id}')" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+            </th>
+            <td>${usernote.noteName}</td>
+            <td>${usernote.noteSelect}</td>
+            <td>
+                <button type="button" class="btn btn-warning text-white" onclick="displayEditModal('${usernote.id}')">
+                    <i class="far fa-edit"></i>
+                </button>
+                <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modal${usernote.id}">
+                    Mostrar
+                </button>
+            </td>
+        </tr>
+        `
+        row.unshift(tr)
+    }
+    notesTable.innerHTML = row.join('')
+}
+
+
+function createInfoModals() {
+    const usersNotes = JSON.parse(localStorage.getItem('usersNotes')) || [];
+    const modals = []
+    for (let i = 0; i < usersNotes.length; i++) {
+        const userNote = usersNotes[i];
+
+
+        const modal = `
+        <!-- Modal${userNote.id} -->
+        <div class="modal fade" id="modal${userNote.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">${userNote.noteName}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <h5>${userNote.noteSelect}</h5>
+                        <hr>
+                        <p>${userNote.noteTextarea}</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        modals.push(modal)
+    }
+    infoModals.innerHTML = modals.join('')
+}
+
+
+
+
+createInfoModals()
+displayNotes()
+
